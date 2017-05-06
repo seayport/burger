5f97917aaa76c9a80165e6ba732edb7ceae95248
Sandy Seay <seaysandra@bellsouth.net>
2017-05-06 10:03:55 -0400

    resending for heroku

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,12 @@ if (process.env.JAWSDB_URL) {
 connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  //port: 3306,
   host: "localhost",
   user: "root",
   password: "1107",
   database: "burgers_db"
   });
+  console.log("hey");
 };
 
 // Make connection.
